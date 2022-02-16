045c1bae4ffb768c423fb153ce221288a829db09
Maheshwar Ligade <maheshwarligade@Maheshwars-MacBook-Pro.local>
2022-02-16 15:35:21 +0530

    nested component

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-
+// special function with name of function start with capital letter
+// stateless functional component
+// always return JSX
+//always return single element
+//
 function Greeting(){
 
-  return <h1>This is my first React Component</h1>
+  return (
+     <div>
+       <h1>This is my first Nested React Component</h1>
+       <Name/>
+       <Message/>
+     </div>
+    );
 }
 
+const Name =() => {
+  return <h4>Hello Maheshwar!</h4>
+}
+
+const Message = () => <p>Welcome to React world</p>
+
+// inject component to index.html to render
 ReactDom.render(<Greeting/>,document.getElementById('root'));
